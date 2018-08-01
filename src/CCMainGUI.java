ff81f3b4d0996ab541f0cda11532748786d4ff86
Michael <michaelgray2@mail.weber.edu>
2018-08-01 15:20:07 -0600

    Created gui template and example

--- src/CCMainGUI.java
+++ src/CCMainGUI.java
@@ -1,3 +1,123 @@
-public class CCMainGUI {
-    // TODO the GUI to be done individually.
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.net.Socket;
+
+public class CCMainGUI extends JFrame implements ActionListener {
+    private static final int serverPort = 8989;
+
+    private BufferedReader in;
+    private PrintWriter out;
+    private JFrame frame = new JFrame("Caverns and Creatures");
+    private JPanel contentPane = new JPanel();
+    private JTextArea chatFieldTXT = new JTextArea(20, 75);;
+    private JScrollPane scrollChatTxt = new JScrollPane(chatFieldTXT,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+    private JTextField submitFieldTXT = new JTextField(75);
+    private JButton sendButton = new JButton("Send");
+    private String userName;
+
+    public CCMainGUI() {
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(50, 50, 805, 650);
+
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+
+        submitFieldTXT.addActionListener(this);
+
+        sendButton.addActionListener(this);
+        sendButton.setEnabled(true);
+
+
+        submitFieldTXT.setBounds(5, 577, 695, 25);
+        sendButton.setBounds(700, 577, 84, 23);
+        scrollChatTxt.setBounds(5,465,780,110);
+        chatFieldTXT.setEditable(false);
+
+        contentPane.add(scrollChatTxt);
+        contentPane.add(submitFieldTXT);
+        contentPane.add(sendButton);
+
+    }
+
+
+    private String getUser() {
+        String username = JOptionPane.showInputDialog(
+                contentPane,
+                "Choose a screen name:",
+                "Screen name selection",
+                JOptionPane.PLAIN_MESSAGE);
+        chatFieldTXT.append("Your username is now " + username + "\n");
+        return username;
+    }
+
+    private String getServerAddress() {
+        String serverName = JOptionPane.showInputDialog(contentPane,
+                "Server name or IP", "ec2-18-207-150-67.compute-1.amazonaws.com");
+        return  serverName;
+    }
+
+    private boolean connectToServer(){
+        String serverAddress = getServerAddress();
+        Socket socket = null;
+
+        try {
+            socket = new Socket(serverAddress, serverPort);
+            in = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            chatFieldTXT.append("Could not connect to " + serverAddress + ". Continuing anyway for testing. \n");
+            return false;
+        }
+        chatFieldTXT.append("Connected to " + serverAddress + "successfully.\n");
+        return true;
+    }
+
+    public void run() {
+        Game game = new Game();
+        boolean isConnected = connectToServer();
+        userName = getUser();
+        //Just a test//
+        ActorPresets actorPresets = new ActorPresets();
+        Actor player1 = actorPresets.playerPresets.get("Fighter");
+        game.addPlayer(userName, player1);
+        chatFieldTXT.append(player1.getType() + "\n");
+        chatFieldTXT.append(player1.getCurrentHitPoints() + "/");
+        chatFieldTXT.append(player1.getMaxHitPoints() + "\n Armor:");
+        chatFieldTXT.append(player1.getArmorClass() + "\n RollCheck: ");
+        chatFieldTXT.append(player1.attackRoll() + "\n");
+
+            // Process all messages from server, according to the protocol.
+            while (true) {
+                //THIS IS WHERE THE SERVER COMMUNICATES WITH THE UI!!!!!!!!!!!!!
+                //Put Handler here...
+            }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String text = submitFieldTXT.getText();
+        chatFieldTXT.append(userName + ": " + text + "\n");
+        submitFieldTXT.selectAll();
+        chatFieldTXT.setCaretPosition(chatFieldTXT.getDocument().getLength());
+    }
+
+    public static void main(String[] args) throws IOException {
+        try {
+            CCMainGUI frame = new CCMainGUI();
+            frame.setVisible(true);
+            frame.run();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
