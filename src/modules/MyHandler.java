1e16a6f12e48b54b2e39a40a5f51e14f901648c1
Gracie <graciedavenport@mail.weber.edu>
2018-08-05 10:42:43 -0600

    laying out possible methods

--- src/modules/MyHandler.java
+++ src/modules/MyHandler.java
@@ -13,6 +13,38 @@ public class MyHandler extends Handler {
             System.out.println(message);
     }
 
+    public void startGame(){
+        /* While loop waiting for player to join,
+        * when player joins  a message is sent to client(s) that says "username" has joined the game
+        * ---What is the condition for the game to start? For now, we can say when the game has three players----
+        * Send message from server to client to update gui - contains player information*/
+    }
+
+    public void addCreature(){
+        //setInitiative();
+
+    }
+
+    public void setInitiative(){
+
+    }
+
+    public void updateBattleReport(){
+
+    }
+
+    public void sendBattleReport(){
+
+    }
+
+    public void updateScoreCard(){
+
+    }
+
+    public void sendScoreCard(){
+
+    }
+
 
     public static void main(String[] args) {
         String portString = "8990";
