6c8ee650ea219c507ba35326cec649f2b8cd61ef
Patrick <patrickleishman@mail.weber.edu>
2018-08-07 23:44:58 -0600

    Implemented game control buttons in gui. Commented out unimplemented JSON methods.

--- src/CCMainGUI.java
+++ src/CCMainGUI.java
@@ -1,41 +1,39 @@
-
 import modules.Actor;
 import modules.ActorPresets;
-import modules.Game;
 import modules.JSONLibrary;
-
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class CCMainGUI extends JFrame implements ActionListener {
     private static final int serverPort = 8989;
+    private JPanel contentPane;
     private BufferedReader in;
     private PrintWriter out;
-    private JFrame frame = new JFrame("Caverns and Creatures");
-    private JPanel contentPane = new JPanel();
-    private JTextArea chatFieldTXT = new JTextArea(20, 75);
-    private JLabel scoreBoardLBL = new JLabel();
-    private JScrollPane scrollChatTxt = new JScrollPane(chatFieldTXT,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-    private JTextField submitFieldTXT = new JTextField(75);
-    private JButton sendButton = new JButton("Send");
-    private JButton attackButton = new JButton("Roll Attack");
-    private JButton damageButton = new JButton("Roll Damage");
+    private JTextArea chatFieldTXT;
+    private JLabel scoreBoardLBL;
+    private JScrollPane scrollChatTxt;
+    private JTextField submitFieldTXT;
+    private JButton sendButton, joinLobbyButton, startGameButton, addCreatureButton, attackButton, damageButton, turnButton;
     private JComboBox<String> playerComboBox;
-    private String username;
-    private String playerCharacter;
+    private String username, playerCharacter, target;
     private Actor playerActor;
-    private int initiative, attackRoll, damageRoll;
-    static final String module = "creaturesAndCaverns";
-
+    private int attackRoll, damageRoll;
 
     public CCMainGUI() {
         setTitle("Caverns and Creatures");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(10, 10, 905, 700);
+
+        contentPane = new JPanel();
+        chatFieldTXT = new JTextArea(20, 75);
+        scrollChatTxt = new JScrollPane(chatFieldTXT,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scoreBoardLBL = new JLabel();
+        submitFieldTXT = new JTextField(75);
 
         contentPane.setBorder(new EmptyBorder(0, 5, 5, 5));
         setContentPane(contentPane);
@@ -43,35 +41,107 @@ public class CCMainGUI extends JFrame implements ActionListener {
 
         scoreBoardLBL.setOpaque(true);
 
+        createGameControls();
+
+        createChatArea();
+    }
+
+    private void createChatArea() {
+        sendButton = new JButton("Send");
         submitFieldTXT.addActionListener(this);
-
-        attackButton.addActionListener(e->{
-            attackRoll = playerActor.rollAttack();
-        });
-
-        damageButton.addActionListener((e->{
-            damageRoll = playerActor.rollDamage();
-        }));
-
 
         sendButton.addActionListener(this);
         sendButton.setEnabled(true);
-        attackButton.setEnabled(true);
         chatFieldTXT.setEditable(false);
-
         scoreBoardLBL.setBounds(0, 0, 880, 140);
-        attackButton.setBounds(5,175,84,23);
+
         submitFieldTXT.setBounds(5, 627, 795, 25);
         sendButton.setBounds(800, 627, 84, 23);
         scrollChatTxt.setBounds(5,515,880,110);
-
 
         contentPane.add(scoreBoardLBL);
         contentPane.add(scrollChatTxt);
         contentPane.add(submitFieldTXT);
         contentPane.add(sendButton);
-        contentPane.add(attackButton);
+    }
 
+    private void createGameControls() {
+        joinLobbyButton = new JButton("Join Game Lobby");
+        startGameButton = new JButton("Start Game");
+        addCreatureButton = new JButton("Add Creature");
+        attackButton = new JButton("Roll Attack");
+        damageButton = new JButton("Roll Damage");
+        playerComboBox = new JComboBox<>();
+        playerComboBox.addItem("--Target--");
+        turnButton = new JButton("Initiate Turn");
+
+        startGameButton.setVisible(false);
+        addCreatureButton.setVisible(false);
+        attackButton.setVisible(false);
+        damageButton.setVisible(false);
+        playerComboBox.setVisible(false);
+        turnButton.setVisible(false);
+
+        joinLobbyButton.addActionListener(e->{
+            joinLobbyButton.setVisible(false);
+            startGameButton.setVisible(true);
+            addCreatureButton.setVisible(true);
+            //sendJson(JSONLibrary.sendJoinGame);
+        });
+
+        startGameButton.addActionListener(e->{
+            sendJson(JSONLibrary.startGame());
+            startGameButton.setEnabled(false);
+            attackButton.setVisible(true);
+            damageButton.setVisible(true);
+            playerComboBox.setVisible(true);
+            turnButton.setVisible(true);
+        });
+
+        attackButton.addActionListener(e->{
+            attackRoll = playerActor.rollAttack();
+            chatFieldTXT.append("Attack Roll: " + attackRoll + "\n");
+            attackButton.setEnabled(false);
+        });
+
+        damageButton.addActionListener(e->{
+            damageRoll = playerActor.rollDamage();
+            chatFieldTXT.append("Damage Roll: " + damageRoll + "\n");
+            damageButton.setEnabled(false);
+        });
+
+        playerComboBox.addActionListener(e->{
+            JComboBox cb = (JComboBox)e.getSource();
+            if(!cb.getSelectedItem().equals("--Target--")){
+                target = (String)cb.getSelectedItem();
+                chatFieldTXT.append("Target : " + target + "\n");
+            }
+        });
+
+        turnButton.addActionListener(e->{
+            if(!attackButton.isEnabled() && !damageButton.isEnabled()){
+                //sendJson(JSONLibrary.sendCombat(username, target, attackRoll, damageRoll));
+                turnButton.setEnabled(false);
+            } else {
+                chatFieldTXT.append("You must roll attack and damage to initiate combat.\n");
+            }
+        });
+
+        joinLobbyButton.setBounds(5, 175, 300, 25);
+        startGameButton.setBounds(5, 175, 150, 25);
+        addCreatureButton.setBounds(155, 175, 150, 25);
+        attackButton.setBounds(5,200,150,25);
+        damageButton.setBounds(155, 200, 150, 25);
+        playerComboBox.setBounds(5, 225, 300, 25);
+        turnButton.setBounds(5, 250, 300, 25);
+
+        contentPane.add(joinLobbyButton);
+        contentPane.add(startGameButton);
+        contentPane.add(addCreatureButton);
+        contentPane.add(attackButton);
+        contentPane.add(damageButton);
+        contentPane.add(playerComboBox);
+        contentPane.add(turnButton);
     }
 
     private String getUser() {
@@ -130,14 +200,15 @@ public class CCMainGUI extends JFrame implements ActionListener {
         return true;
     }
 
-    private void displayScoreBoard(Game game) {
+    //Takes arrays of player names and player scoreboards from game running on server
+    private void displayScoreBoard(ArrayList<String> playerNames, ArrayList<String> playerStats) {
         String scoreBoard = "<HTML><TABLE ALIGN=TOP BORDER=0  cellspacing=2 cellpadding=2><TR>";
-        for(String actorName: game.getNames()){
+        for(String actorName: playerNames){
             scoreBoard += "<TH><H2>" + actorName + "</H2></TH>";
         }
         scoreBoard += "</TR><TR>";
         int count = 0;
-        for(String anActor: game.getScoreBoard()){
+        for(String anActor: playerStats){
             if(count % 2 == 0){
                 scoreBoard += "<TD BGCOLOR=" + anActor + ">";
             }
@@ -166,8 +237,6 @@ public class CCMainGUI extends JFrame implements ActionListener {
 
         }
     }
-
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
