90e1eabdd9b304ab7c97968e52f77638f3db8578
Michael <michaelgray2@mail.weber.edu>
2018-08-08 20:43:36 -0600

    gui intergration

--- src/CCMainGUI.java
+++ src/CCMainGUI.java
@@ -4,33 +4,40 @@ import modules.ActorPresets;
 import modules.Game;
 import org.json.JSONObject;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Timer;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.locks.ReentrantLock;
 
+import static com.sun.javafx.scene.control.skin.Utils.getResource;
 import static java.lang.Thread.sleep;
 
 public class CCMainGUI extends JFrame implements ActionListener {
     private static final int serverPort = 8989;
+
     private Game game = new Game();
     private BufferedReader in;
     private PrintWriter out;
+
+    private ImageIcon sword = createImageIcon("sword.png");
+
     private JFrame frame = new JFrame("Caverns and Creatures");
-    private JPanel contentPane = new JPanel();
+    private JLayeredPane contentPane = new JLayeredPane();
     private JTextArea chatFieldTXT = new JTextArea(20, 75);
     private JLabel scoreBoardLBL = new JLabel();
+    private JLabel imgBackground = new JLabel();
+    private JLabel topBackground = new JLabel();
     private JScrollPane scrollChatTxt = new JScrollPane(chatFieldTXT,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
     private JTextField submitFieldTXT = new JTextField(75);
     private JButton sendButton = new JButton("Send");
-    private JButton attackButton = new JButton("Attack");
+    private JButton attackButton = new JButton("Attack", sword);
     private JButton addCreatureButton = new JButton("Add Creature");
     private String username;
     private String playerCharacter;
@@ -42,18 +49,22 @@ public class CCMainGUI extends JFrame implements ActionListener {
         setTitle("Caverns and Creatures");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(10, 10, 905, 700);
-
+        setIconImage((createImageIcon("ccicon.png")).getImage());
         contentPane.setBorder(new EmptyBorder(0, 5, 5, 5));
+        contentPane.setBackground(new Color( 216,234,240));
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        scoreBoardLBL.setOpaque(true);
+        contentPane.setOpaque(true);
+        imgBackground.setOpaque(true);
+        scoreBoardLBL.setOpaque(false);
+
 
         submitFieldTXT.addActionListener(this);
         attackButton.addActionListener(new java.awt.event.ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                chatFieldTXT.append("Attack");
+                chatFieldTXT.append("Attack\n");
             }
         });
         addCreatureButton.addActionListener(evt -> executor.submit(() -> {
@@ -67,27 +78,48 @@ public class CCMainGUI extends JFrame implements ActionListener {
             }
         }));
 
+        imgBackground.setIcon(createImageIcon("dark_field.jpg"));
+        topBackground.setIcon(createImageIcon("dragonbackground.png"));
+        scoreBoardLBL.setForeground(Color.WHITE);
         sendButton.addActionListener(this);
         sendButton.setEnabled(true);
         attackButton.setEnabled(true);
+        attackButton.setBorder(BorderFactory.createEmptyBorder());
+        attackButton.setForeground(new Color( 161,81,55));
+        attackButton.setVerticalTextPosition(AbstractButton.BOTTOM);
+        attackButton.setHorizontalTextPosition(AbstractButton.CENTER);
         addCreatureButton.setEnabled(true);
         chatFieldTXT.setEditable(false);
 
+        topBackground.setBounds(0, 0, 905, 140);
+        imgBackground.setBounds(0, 0, 905, 700);
         scoreBoardLBL.setBounds(0, 0, 905, 140);
-        attackButton.setBounds(5,175,84,23);
-        addCreatureButton.setBounds(5,200,175,23);
+        attackButton.setBounds(0,140,80,80);
+        addCreatureButton.setBounds(5,492,175,23);
         submitFieldTXT.setBounds(5, 627, 795, 25);
         sendButton.setBounds(800, 627, 84, 23);
         scrollChatTxt.setBounds(5,515,880,110);
 
+        contentPane.add(topBackground,JLayeredPane.PALETTE_LAYER);
+        contentPane.add(imgBackground,JLayeredPane.DEFAULT_LAYER);
+        contentPane.add(scoreBoardLBL,JLayeredPane.MODAL_LAYER);
+        contentPane.add(scrollChatTxt,JLayeredPane.MODAL_LAYER);
+        contentPane.add(submitFieldTXT,JLayeredPane.MODAL_LAYER);
+        contentPane.add(sendButton,JLayeredPane.MODAL_LAYER);
+        contentPane.add(attackButton,JLayeredPane.MODAL_LAYER);
+        contentPane.add(addCreatureButton,JLayeredPane.MODAL_LAYER);
 
-        contentPane.add(scoreBoardLBL);
-        contentPane.add(scrollChatTxt);
-        contentPane.add(submitFieldTXT);
-        contentPane.add(sendButton);
-        contentPane.add(attackButton);
-        contentPane.add(addCreatureButton);
+    }
 
+    /** Returns an ImageIcon, or null if the path was invalid. */
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = CCMainGUI.class.getClassLoader().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
     }
 
     private String getUser() {
@@ -163,7 +195,7 @@ public class CCMainGUI extends JFrame implements ActionListener {
     private String displayScoreBoard(Game game) {
         String scoreBoard = "<HTML><TABLE ALIGN=TOP BORDER=0  cellspacing=2 cellpadding=2><TR>";
         for(String actorName: game.getNames()){
-            scoreBoard += "<TH><H2>" + actorName + "</H2></TH>";
+            scoreBoard += "<TH BGCOLOR=BLACK><H2>" + actorName + "</H2></TH>";
         }
         scoreBoard += "</TR><TR>";
         int count = 0;
@@ -200,6 +232,8 @@ public class CCMainGUI extends JFrame implements ActionListener {
         Actor player1 = actorPresets.playerPresets.get(playerCharacter);
         game.addPlayer(username, player1.getType());
         game.addRandomMonster();
+
+        scoreBoardLBL.setText(displayScoreBoard(game));
 
         //for titles of UI
         for(String usernames: game.getNames()){
