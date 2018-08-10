42fc946ae89c3ff293a6bec3f6c46f901b437120
Patrick <patrickleishman@mail.weber.edu>
2018-08-09 20:01:03 -0600

    Preparing for merge.

--- src/CCMainGUI.java
+++ src/CCMainGUI.java
@@ -470,6 +470,7 @@ public class CCMainGUI extends JFrame implements ActionListener {
         chatFieldTXT.append(username + ": " + text + "\n");
         submitFieldTXT.selectAll();
         chatFieldTXT.setCaretPosition(chatFieldTXT.getDocument().getLength());
+
     }
 
     public static void main(String[] args) throws IOException {
